b1a99f116a1d26f510042ff9f4623cca193dc2e4
LockyStar <Zhabrevalexander@yandex.ru>
2015-11-01 20:05:39 +0300

    first

---
+++ Assets/WorldObject/WorldObject.cs
@@ -0,0 +1,127 @@
+﻿using UnityEngine;
+using RTS;
+
+public class WorldObject : MonoBehaviour
+{
+
+    //Public variables
+    public string objectName;
+    public Texture2D buildImage;
+    public int cost, sellValue, hitPoints, maxHitPoints;
+
+    //Variables accessible by subclass
+    protected Player player;
+    protected string[] actions = { };
+    protected bool currentlySelected = false;
+    protected Bounds selectionBounds;
+    protected Rect playingArea = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+
+    /*** Game Engine methods, all can be overridden by subclass ***/
+
+    protected virtual void Awake()
+    {
+        selectionBounds = ResourceManager.InvalidBounds;
+        CalculateBounds();
+    }
+
+    protected virtual void Start()
+    {
+        player = transform.root.GetComponentInChildren<Player>();
+    }
+
+    protected virtual void Update()
+    {
+
+    }
+
+    protected virtual void OnGUI()
+    {
+        if (currentlySelected) DrawSelection();
+    }
+
+    /*** Public methods ***/
+
+    public void SetSelection(bool selected, Rect playingArea)
+    {
+        currentlySelected = selected;
+        if (selected) this.playingArea = playingArea;
+    }
+
+    public string[] GetActions()
+    {
+        return actions;
+    }
+
+    public virtual void PerformAction(string actionToPerform)
+    {
+        //it is up to children with specific actions to determine what to do with each of those actions
+    }
+
+    public virtual void MouseClick(GameObject hitObject, Vector3 hitPoint, Player controller)
+    {
+        //only handle input if currently selected
+        if (currentlySelected && hitObject && hitObject.name != "Ground")
+        {
+            WorldObject worldObject = hitObject.transform.root.GetComponent<WorldObject>();
+            //clicked on another selectable object
+            if (worldObject) ChangeSelection(worldObject, controller);
+        }
+    }
+
+    public void CalculateBounds()
+    {
+        selectionBounds = new Bounds(transform.position, Vector3.zero);
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            selectionBounds.Encapsulate(r.bounds);
+        }
+    }
+
+    /*** Private worker methods ***/
+
+    private void ChangeSelection(WorldObject worldObject, Player controller)
+    {
+        //this should be called by the following line, but there is an outside chance it will not
+        SetSelection(false, playingArea);
+        if (controller.SelectedObject) controller.SelectedObject.SetSelection(false, playingArea);
+        controller.SelectedObject = worldObject;
+        worldObject.SetSelection(true, controller.hud.GetPlayingArea());
+    }
+
+    private void DrawSelection()
+    {
+        GUI.skin = ResourceManager.SelectBoxSkin;
+        Rect selectBox = WorkManager.CalculateSelectionBox(selectionBounds, playingArea);
+        //Draw the selection box around the currently selected object, within the bounds of the playing area
+        GUI.BeginGroup(playingArea);
+        DrawSelectionBox(selectBox);
+        GUI.EndGroup();
+    }
+
+    /* Internal worker methods that can be accessed by subclass */
+
+    protected virtual void DrawSelectionBox(Rect selectBox)
+    {
+        GUI.Box(selectBox, "");
+    }
+
+    public virtual void MouseClick(GameObject hitObject, Vector3 hitPoint, Player controller)
+    {
+        //only handle input if currently selected
+        if (currentlySelected && hitObject && hitObject.name != "Ground")
+        {
+            WorldObject worldObject = hitObject.transform.root.GetComponent<WorldObject>();
+            //clicked on another selectable object
+            if (worldObject) ChangeSelection(worldObject, controller);
+        }
+    }
+
+    private void ChangeSelection(WorldObject worldObject, Player controller)
+    {
+        //this should be called by the following line, but there is an outside chance it will not
+        SetSelection(false);
+        if (controller.SelectedObject) controller.SelectedObject.SetSelection(false);
+        controller.SelectedObject = worldObject;
+        worldObject.SetSelection(true);
+    }
+}
